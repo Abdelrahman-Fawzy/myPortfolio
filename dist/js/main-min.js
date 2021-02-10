f4a116dd2159abec4b8c7776d8e80b4554254543
Abdelrahman-Fawzy <abdelrahmanawzy98@gmail.com>
2021-02-10 20:58:32 +0200

    add portfolio intro

---
+++ dist/js/main-min.js
@@ -0,0 +1,1 @@
+$(function(){$(".intro").height($(window).height()),$(window).resize(function(){$(".intro").height($(window).height())}),$(".toggle").click(function(){$(this).removeClass("fa-toggle-of").toggleClass("fa-toggle-on"),$("aside").toggleClass("noSideBar"),$(".content-area").toggleClass("noSideBar")})});
