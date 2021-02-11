4524beafe1858b427c43cb7d5ee73f3206c131ae
Abdelrahman-Fawzy <abdelrahmanawzy98@gmail.com>
2021-02-11 16:44:29 +0200

    add about and experiences sections

--- stage/js/main.js
+++ stage/js/main.js
@@ -9,4 +9,16 @@ $(function () {
         $('aside').toggleClass('noSideBar')
         $('.content-area').toggleClass('noSideBar')
     })
+
+    $('.about').click(function () {
+        $('html, body').animate({
+            scrollTop: $('.about-us').offset().top
+        }, 1000);
+    })
+
+    $('.experience').click(function () {
+        $('html, body').animate({
+            scrollTop: $('.experiences').offset().top
+        }, 1000);
+    })
 })
