f3bcbf7b8300c62db0dbe31ae564b0e4f1d79681
Abdelrahman-Fawzy <abdelrahmanawzy98@gmail.com>
2021-02-13 00:00:57 +0200

    adding projects section

--- stage/js/main.js
+++ stage/js/main.js
@@ -6,8 +6,17 @@ $(function () {
 
     $('.toggle').click(function () {
         $(this).removeClass('fa-toggle-of').toggleClass('fa-toggle-on')
-        $('aside').toggleClass('noSideBar')
-        $('.content-area').toggleClass('noSideBar')
+        $('aside').toggleClass('showSideBar')
+    })
+
+    $('.card-image, .card-title').click(function () {
+        let cardInfo = `.card-info-${$(this).attr('data-value')}`
+        $(`.projects .card ${cardInfo}`).slideDown("slow", function () {
+            console.log($(this))
+            $(`.projects .card ${cardInfo} .fa-times`).click(function () {
+                $(`.projects .card ${cardInfo}`).slideUp("slow")
+            })
+        });
     })
 
     $('.about').click(function () {
@@ -19,6 +28,12 @@ $(function () {
     $('.experience').click(function () {
         $('html, body').animate({
             scrollTop: $('.experiences').offset().top
+        }, 1000);
+    })
+
+    $('.project').click(function () {
+        $('html, body').animate({
+            scrollTop: $('.projects').offset().top
         }, 1000);
     })
 })
