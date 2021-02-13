d4cc75f4b726cb910e653884ec37dcfdd48a504a
Abdelrahman-Fawzy <abdelrahmanawzy98@gmail.com>
2021-02-13 18:12:47 +0200

    add skills, education and awards sections

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,14 +1,19 @@
 $(function() {
+  //nice scroll
+  $('html, body').niceScroll();
+
   $('.intro').height($(window).height())
   $(window).resize(function() {
     $('.intro').height($(window).height())
   })
 
+  //toggle side bar
   $('.toggle').click(function() {
     $(this).removeClass('fa-toggle-of').toggleClass('fa-toggle-on')
     $('aside').toggleClass('showSideBar')
   })
 
+  // show project info
   $('.card-image, .card-title').click(function() {
     let cardInfo = `.card-info-${$(this).attr('data-value')}`
     $(`.projects .card ${cardInfo}`).slideDown("slow", function() {
@@ -19,21 +24,34 @@ $(function() {
     });
   })
 
-  $('.about').click(function() {
+  // sidebar links
+  $('aside ul li a').click(function() {
     $('html, body').animate({
-      scrollTop: $('.about-us').offset().top
+      scrollTop: $(`.content-area .${$(this).attr('class')}`).offset().top
     }, 1000);
   })
 
-  $('.experience').click(function() {
-    $('html, body').animate({
-      scrollTop: $('.experiences').offset().top
-    }, 1000);
-  })
+  // active side bar link and to top button
+  $(window).scroll(function() {
+    let windowHeight = $(window).scrollTop()
+    $('.content-area').children().each(function() {
+      let asideClass = `${$(this).attr('class')}`
+      if (windowHeight >= $(this).offset().top) {
+        $(`aside ul li .${asideClass}`).addClass('active').parent().siblings().children().removeClass('active')
+      } else {
+        $(`aside ul li .${asideClass}`).removeClass('active')
+      }
+    })
 
-  $('.project').click(function() {
+    if (windowHeight > 500) {
+      $('.toTop').fadeIn(100)
+    } else {
+      $('.toTop').fadeOut(100)
+    }
+  })
+  $('.toTop').click(function() {
     $('html, body').animate({
-      scrollTop: $('.projects').offset().top
-    }, 1000);
+      scrollTop: 0
+    }, 800);
   })
 })
